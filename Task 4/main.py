07488c4c83f4caec7a01bf58877a232801dbca95
Artem Pustynniy <144416748+ArtemPustunniy@users.noreply.github.com>
2024-10-01 20:52:48 +0300

    Update main.py

--- Task 4/main.py
+++ Task 4/main.py
@@ -1,1 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import MainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.setWindowTitle("Visualization of the movement")
+    window.setGeometry(450, 150, 1000, 800)
+    window.show()
+    sys.exit(app.exec_())
+
 
