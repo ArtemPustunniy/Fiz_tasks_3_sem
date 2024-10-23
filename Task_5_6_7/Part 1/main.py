d58482518c98ff7edffe1977a347245324c27f96
Artem Pustynniy <144416748+ArtemPustunniy@users.noreply.github.com>
2024-10-23 09:43:36 +0300

    Update main.py

--- Task_5_6_7/Part 1/main.py
+++ Task_5_6_7/Part 1/main.py
@@ -1,1 +1,12 @@
+# main.py
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import ProjectileApp  # Import the ProjectileApp class
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = ProjectileApp()  # Create an instance of the application
+    ex.show()  # Show the application window
+    sys.exit(app.exec_())  # Start the application event loop
+
 
