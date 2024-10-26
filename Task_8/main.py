641f00145292aa2eb776e1044e7908d1ba5456c1
Artem Pustynniy <144416748+ArtemPustunniy@users.noreply.github.com>
2024-10-26 13:52:37 +0300

    Update main.py

--- Task_8/main.py
+++ Task_8/main.py
@@ -1,1 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui import SimulationApp
 
+if __name__ == "__main__":
+    """
+    Основной блок для запуска приложения PyQt5.
+    """
+    app = QApplication(sys.argv)
+    window = SimulationApp()
+    window.show()
+    sys.exit(app.exec_())
